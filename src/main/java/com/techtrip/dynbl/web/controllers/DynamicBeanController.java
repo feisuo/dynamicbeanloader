182b1a6fa0926080d6b6ac8779ad9071ec279a94
Terry Trippany <trip@techtrip.co>
2014-03-11 14:41:29 -0500

    Fixed logging, added new classes in scripts

--- src/main/java/com/techtrip/dynbl/web/controllers/DynamicBeanController.java
+++ src/main/java/com/techtrip/dynbl/web/controllers/DynamicBeanController.java
@@ -1,5 +1,7 @@
 package com.techtrip.dynbl.web.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.CachedIntrospectionResults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,39 +15,50 @@ import com.techtrip.spring.beans.factory.ContextAwareBeanFactory;
 @Controller
 public class DynamicBeanController {
 
-	@Autowired(required = true)
-	private ContextAwareBeanFactory beanFactory;
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	@ResponseBody
-	public String home() {
-		return "You are home!!";
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/registerBean")
-	@ResponseBody
-	private String registerBean(@RequestParam(value = "beanName", required = true) String beanName,@RequestParam(value = "beanType", required = true) String beanType) {
-		try {
-			Class<?> c;
+    private static Logger LOGGER = LoggerFactory.getLogger(DynamicBeanController.class);
+    
+    @Autowired(required = true)
+    private ContextAwareBeanFactory beanFactory;
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String home() {
+            return "You are home!!";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/registerBean")
+    @ResponseBody
+    private String registerBean(@RequestParam(value = "beanName", required = true) String beanName,@RequestParam(value = "beanType", required = true) String beanType) {
+            try {
+                    Class<?> c;
 
-			c = Class.forName(beanType);
+                    c = Class.forName(beanType);
 
-			CachedIntrospectionResults.clearClassLoader(c.getClassLoader());
-			
-			beanFactory.registerBean(c, beanName, "prototype", false, true);
-			
-			/*
-			 * 
-			 * 			if (ClassUtils.isCacheSafe(beanClass, CachedIntrospectionResults.class.getClassLoader()) ||
-					isClassLoaderAccepted(beanClass.getClassLoader())) {
-			 */
-			//CachedIntrospectionResults.clearClassLoader(c.getClassLoader());
-			
-			return String.format("The following bean was registered and instantiated: Bean Name:  %s, Bean Type: %s",beanName, beanFactory.getBean(beanName).toString());
-		} catch (Exception e) {
-			return String
-					.format("Unable to create bean of type %s with the following error:</br>%s!",
-							beanType, e.getMessage());
-		}
-	}
+                    CachedIntrospectionResults.clearClassLoader(c.getClassLoader());
+                    
+                    beanFactory.registerBean(c, beanName, "prototype", false, true);
+                    
+                    
+                    String ret = String.format("The following bean was registered and instantiated: Bean Name:  %s, Bean Type: %s",beanName, beanFactory.getBean(beanName).toString());
+                    		
+                    if (LOGGER.isDebugEnabled()) {
+                            LOGGER.debug(beanType);
+                    }
+                    
+                    /*
+                    * 
+                    *                       if (ClassUtils.isCacheSafe(beanClass, CachedIntrospectionResults.class.getClassLoader()) ||
+                                    isClassLoaderAccepted(beanClass.getClassLoader())) {
+                    */
+                    //CachedIntrospectionResults.clearClassLoader(c.getClassLoader());
+                    
+                    
+                    
+                    return ret;
+            } catch (Exception e) {
+                    return String
+                                    .format("Unable to create bean of type %s with the following error:</br>%s!",
+                                                    beanType, e.getMessage());
+            }
+    }
 }
